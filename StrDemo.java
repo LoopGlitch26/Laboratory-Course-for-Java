ab6b1482335d6667f1dc1dfbb07bbf1c4baaeb9c
Bravish Ghosh <53336715+LoopGlitch26@users.noreply.github.com>
2020-04-01 08:29:13 +0530

    Update StrDemo.java

--- StrDemo.java
+++ StrDemo.java
@@ -11,24 +11,21 @@ public class StrDemo
     final String CAT = "cat";
 
     System.out.println("Enter a string.");
-    inLine = inData.nextLine();
+    inLine = inData.nextLine(); // suppose that the input is "How much is that doggie in the window?"
     // Examples of various string operations
 
-    System.out.println(inLine.length());
-    System.out.println(inLine.indexOf("the"));
-    System.out.println(inLine.indexOf(CAT));
-    System.out.println(inLine.substring(17, 23));
-    System.out.println(inLine.substring(17, 17));
-    System.out.println(inLine.substring(17, 23).length());
-System.out.println("" + inLine.charAt(0) + 
-inLine.charAt(1));
-System.out.println("How much would you would pay " +
-  "for the doggie?");
+    System.out.println(inLine.length()); // total number of characters in the string, starts with 1
+    System.out.println(inLine.indexOf("the")); // counting starts from 0, so t in the is at 27th position
+    System.out.println(inLine.indexOf(CAT)); // as the string or character does'nt occur in the string object, so output is -1
+    System.out.println(inLine.substring(17, 23)); //starts with 0
+    System.out.println(inLine.substring(17, 17)); // empty string is output
+    System.out.println(inLine.substring(17, 23).length()); // a method can be applied to result of another method, output 6
+System.out.println("" + inLine.charAt(0) + inLine.charAt(1)); // character at the position specified, output Ho, i.e starts from 1 like length()
+System.out.println("How much would you would pay " + "for the doggie?");
 System.out.println("(in whole dollars please)");
 dollars = inData.nextInt();
-System.out.println("Would you really pay $" + dollars + 
-"?");
-System.out.println(("  trim test ").trim());
+System.out.println("Would you really pay $" + dollars + "?");
+System.out.println(("  trim test ").trim()); // trim() removes the blanks on both sides
 inData.close();
   }
 }
