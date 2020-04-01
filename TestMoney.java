84f580c14e5a4044a9589f85dda57b1591380cc4
Bravish Ghosh <53336715+LoopGlitch26@users.noreply.github.com>
2020-04-01 10:28:09 +0530

    Update TestMoney.java

--- TestMoney.java
+++ TestMoney.java
@@ -4,22 +4,18 @@ public class TestMoney
   public static void main(String[] args)
   {
     Money myMoney = new Money();
-	  System.out.println(myMoney.getDollars() + " dollars " 
-      + mMoney.getCents() + " cents");
+	  System.out.println(myMoney.getDollars() + " dollars " + mMoney.getCents() + " cents");
     System.out.println(myMoney);
     Money yourMoney = new Money(33, 44);
-	  System.out.println(yourMoney.getDollars() + " dollars " 
-      + yourMoney.getCents() + " cents");
+	  System.out.println(yourMoney.getDollars() + " dollars " + yourMoney.getCents() + " cents");
     System.out.println(yourMoney);
 	  Money otherMoney = new Money(4, 66);
 	  Money answer = new Money();
 	  Money ourMoney = new Money();
 	  ourMoney = myMoney.add(yourMoney);
-	  System.out.println(myMoney + " + " + yourMoney 
-      + " = " + ourMoney);
+	  System.out.println(myMoney + " + " + yourMoney + " = " + ourMoney);
 	  answer = yourMoney.add(otherMoney);	
-	  System.out.println(yourMoney + " + " + otherMoney 
-      + " = " + answer);
-  }
+	  System.out.println(yourMoney + " + " + otherMoney + " = " + answer);
+  } 
 }
 
